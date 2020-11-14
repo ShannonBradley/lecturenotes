caa9791c4ae49c98e39c5878b8e49bfa7fe15988
Shannon Bradley <mrktguru@gmail.com>
2020-11-14 13:41:32 -0600

    first commit

---
+++ challenge1.js
@@ -0,0 +1,14 @@
+var str = "Shannon";
+console.log(str.length);
+
+var name = "Jenn";
+console.log(name.length);
+
+if (str.length > name.length){
+    console.log("My name is longer than Jenn's");
+}
+
+
+
+
+
---
+++ challenge2.js
@@ -0,0 +1,38 @@
+let myString = "this is a string";
+let myNumber = 10;
+let myBoolean = false;
+let myArray = [10, "string", []];
+let myObject = {firstkey: "a strong"};
+let myUndefined = undefined;
+let myNull = null;
+
+console.log(typeof myBoolean);
+
+let myobject = {
+    string: "String",
+    number: 15m,
+    boolean: true,
+    object: {},
+}
+
+console.log(typeof myObject.boolean)
+
+
+
+let dognameone = "Emilee";
+let dognametwo = "Lucy";
+console.log(dognameone, dognametwo)
+
+let dogage = 6;
+console.log(dogage)
+
+var brown = "true";
+var black = "false";
+console.log(black)
+
+let myObject = {dogtype: "mutt"}
+
+
+
+
+
